575fec8ee3b39dcefa90357df879fd8bdb9b37af
Mohsun88 <39255245+Mohsun88@users.noreply.github.com>
2018-08-07 13:31:48 +0400

    Update python.py

--- python.py
+++ python.py
@@ -120,5 +120,81 @@ lin_rmse = np.sqrt(lin_mse)
 lin_rmse
 
           
+from sklearn.tree import DecisionTreeRegressor
+
+tree_reg = DecisionTreeRegressor(random_state=42)
+tree_reg.fit(X_train, y_train)
+          
+
+tree_scores = cross_val_score(tree_reg, X_train, y_train,
+                             scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-tree_scores)
+display_scores(tree_rmse_scores)
+          
+price_predictions = tree_reg.predict(X_train)
+tree_mse = mean_squared_error(y_train, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+tree_rmse
+          
+          
+from sklearn.ensemble import RandomForestRegressor
+
+forest_reg = RandomForestRegressor(random_state=42)
+forest_reg.fit(X_train, y_train)
+          
+
+housing_predictions = forest_reg.predict(X_train)
+forest_mse = mean_squared_error(y_train, housing_predictions)
+forest_rmse = np.sqrt(forest_mse)
+forest_rmse
+          
+          
+forest_scores = cross_val_score(forest_reg, X_train, y_train,
+                             scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-forest_scores)
+display_scores(tree_rmse_scores)
+          
+         
+from sklearn.model_selection import GridSearchCV
+
+param_grid = [
+    # try 12 (3×4) combinations of hyperparameters
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    # then try 6 (2×3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+  ]
+
+forest_reg = RandomForestRegressor(random_state=42)
+# train across 5 folds, that's a total of (12+6)*5=90 rounds of training 
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                           scoring='neg_mean_squared_error')
+grid_search.fit(X_train, y_train)
+          
+          
+grid_search.best_params_
+          
+grid_search.best_estimator_
+          
+cvres = grid_search.cv_results_
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print(np.sqrt(-mean_score), params)
+          
+#154125.671031 result is the lowest RMSE, so we take 'max_features': 8 and n_estimators': 30.
+          
+          
+feature_importances = grid_search.best_estimator_.feature_importances_
+sorted(zip(feature_importances, X), reverse=True)
+          
+final_model = grid_search.best_estimator_
+
+X_test_prepared = Scaler.fit_transform(X_test)
+final_predictions = final_model.predict(X_test_prepared)
+
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
+          
+final_rmse
+          
+
           
   
